483c629b7d7b4bb68a6ab0284945c5df7d0bce8e
Gojda <ivansoligorsk22@gmail.com>
2025-08-03 20:23:03 +0300

    изменил регистр в импорте

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from './components/Footer'
 import { Outlet } from "react-router-dom";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
